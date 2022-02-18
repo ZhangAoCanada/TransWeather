aeb8c713cc6f99559b469e4bb565daf0daa466b9
ZhangAoCanada <zhangaocanada@gmail.com>
2022-02-18 14:01:41 +0800

    adjust lr

--- utils.py
+++ utils.py
@@ -133,7 +133,7 @@ def print_log(epoch, num_epochs, one_epoch_time, train_psnr, val_psnr, val_ssim,
 def adjust_learning_rate(optimizer, epoch,  lr_decay=0.95):
 
     # --- Decay learning rate --- #
-    step = 1
+    step = 2
 
     if not epoch % step and epoch > 0:
         for param_group in optimizer.param_groups:
